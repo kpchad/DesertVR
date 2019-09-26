b701cebfd8c5f0b191481fa0383132d53825f8a3
Kyle Chadwick <kylepchad@gmail.com>
2019-09-25 23:12:43 -0700

    randomy appearing animated critters

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -13,20 +13,44 @@ public class Client : MonoBehaviour
     private int m_IncrementorLizard = 0;
     private int m_IncrementorSnake = 0;
 
+    int x;
+    int y;
+    int z;
+    int count = 0;
+
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L))
+        
+        if (count == 500)
         {
             m_Spawn = m_Spawner.SpawnEnemy(m_Lizard);
             m_Spawn.name = "Lizard_Clone_" + ++m_IncrementorLizard;
-            m_Spawn.transform.Translate(Vector3.forward * m_IncrementorLizard * 2.5f);
+
+            // set coordinate range for objects to spawn within
+            x = Random.Range(-150, 150);
+            y = 5;
+            z = Random.Range(-150, 150);
+            Vector3 pos = new Vector3(x, y, z);
+
+            m_Spawn.transform.Translate(pos);
+            Debug.Log("X: " + x + " Y: " + y + " Z: " + z);
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (count == 1000)
         {
             m_Spawn = m_Spawner.SpawnEnemy(m_Snake);
             m_Spawn.name = "Snake_Clone_" + ++m_IncrementorSnake;
-            m_Spawn.transform.Translate(Vector3.forward * m_IncrementorSnake * 2.5f);
+
+            // set coordinate range for objects to spawn within
+            x = Random.Range(-150, 150);
+            y = 5;
+            z = Random.Range(-150, 150);
+            Vector3 pos = new Vector3(x, y, z);
+
+            m_Spawn.transform.Translate(pos);
+            Debug.Log("X: " + x + " Y: " + y + " Z: " + z);
+            count = 0;
         }
+        count++;
     }
 }
